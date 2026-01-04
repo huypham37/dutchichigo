305dae34cde2d3aefe4ccdc7d45dc7d62c2ae01b
huypham37 <phamhuy307@gmail.com>
2026-01-04 18:11:23 +0100

    Initial Commit

---
+++ Dutchichigo/DutchichigoApp.swift
@@ -0,0 +1,32 @@
+//
+//  DutchichigoApp.swift
+//  Dutchichigo
+//
+//  Created by charles on 04/01/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DutchichigoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
